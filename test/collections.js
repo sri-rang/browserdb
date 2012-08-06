f0f77ecb21fd7183d28fcc9c618914b6acd0edc0
Srirangan <srirangan@gmail.com>
2012-08-06 10:44:13 +0530

    Tests added for DB and Collections

---
+++ test/collections.js
@@ -0,0 +1,12 @@
+new BrowserDb({
+  db:"testDB",
+  collections:["one", "two", "three"]
+}, function (error, testDb) {
+
+  test("Collection APIs must be exposed in db instance", function() {
+    ok(testDb.one, "testDb.one must exist");
+    ok(testDb.two, "testDb.two must exist");
+    ok(testDb.three, "testDb.three must exist");
+  });
+
+});
---
+++ test/db.js
@@ -0,0 +1,14 @@
+new BrowserDb({
+  db:"testDB",
+  collections: []
+}, function (error, testDb) {
+
+  test("No errors connecting to db", function() {
+    ok(!error, "Error must be undefined")
+  });
+
+  test("Db instance created", function() {
+    ok(testDb, "testDb instance")
+  });
+
+});
